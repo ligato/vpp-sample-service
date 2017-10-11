3c476c2a1fa33a99034fa871e4fb20baff8c341a
Filip Gschwandtner <filip.gschwandtner@pantheon.tech>
2017-10-11 11:59:08 +0200

    fixed example run for bgp-agent update

--- scripts/run_vpp_l3_bgp_routes.sh
+++ scripts/run_vpp_l3_bgp_routes.sh
@@ -3,22 +3,22 @@ LIGATO_BGP_AGENT="/vendor/github.com/ligato/bgp-agent"
 source .$LIGATO_BGP_AGENT/scripts/testOuput.sh
 exitCode=0
 
-VENDOR_BGP_AGENT_DOCKER=$LIGATO_BGP_AGENT"/docker"
-VENDOR_BGP_AGENT_RR_DOCKER=$VENDOR_BGP_AGENT_DOCKER"/gobgp_route_reflector"
+VENDOR_BGP_AGENT_RR_DOCKER=$LIGATO_BGP_AGENT"/docker/gobgp_route_reflector"
+VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS=$VENDOR_BGP_AGENT_RR_DOCKER"/usage_scripts"
 
 #####Setup Docker Network ##################################################
-.$VENDOR_BGP_AGENT_RR_DOCKER/create-ligato-network-for-docker.sh
+.$VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS/create-ligato-network-for-docker.sh
 #####Download Dockers ##################################################
-.$VENDOR_BGP_AGENT_DOCKER/gobgp_for_rr/pull-docker.sh
+.$VENDOR_BGP_AGENT_RR_DOCKER/pull-docker.sh
 ./docker/dev_bgp_to_vpp/pull-docker.sh
 
 #####Run VPP Dockers ##################################################
-.$VENDOR_BGP_AGENT_RR_DOCKER/start-routereflector.sh gobgp-client-in-docker
+.$VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS/start-routereflector.sh gobgp-client-in-docker
 ./docker/dev_bgp_to_vpp/scripts/start-bgp-to-vpp.sh
 
 
 #####Add Path##################################################
-.$VENDOR_BGP_AGENT_RR_DOCKER/addPath.sh &
+.$VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS/addPath.sh &
 #####Start Vpp##################################################
 ./docker/dev_bgp_to_vpp/scripts/start-vpp.sh &
 #
@@ -39,8 +39,8 @@ expected=("101.0.10.1/32
 testOutput "$(less fib)" "${expected}"
 
 #####Close Dockers##################################################
-.$VENDOR_BGP_AGENT_RR_DOCKER/stop-routereflector.sh
+.$VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS/stop-routereflector.sh
 ./docker/dev_bgp_to_vpp/scripts/stop-bgp-to-vpp.sh
-.$VENDOR_BGP_AGENT_RR_DOCKER/remove-ligato-network-for-docker.sh
+.$VENDOR_BGP_AGENT_RR_DOCKER_SCRIPTS/remove-ligato-network-for-docker.sh
 ##########################################################################
 exit ${exitCode}
